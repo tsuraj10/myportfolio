5b7c95c793f9c62cd39881d48845f3b807bc6c36
Suraj Thapa <suraj15025@gmail.com>
2025-02-03 09:27:54 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base : '/myportfolio/',
+  base : '/',
   plugins: [react(),tailwindcss()],
 })
